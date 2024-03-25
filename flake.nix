9618c107f02adf29678e6e31fd16a238745b62fe
YawKar <yawkarpov@gmail.com>
2024-03-25 18:53:25 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "summarized_and_tokenized_by_gpt2";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { system = "${system}"; config.allowUnfree = true; };
+      OPENGL_DRIVER="/run/opengl-driver/lib";
+    in
+    {
+      devShells.${system}.default =
+        pkgs.mkShell
+          {
+            LD_LIBRARY_PATH =
+              "${OPENGL_DRIVER}:" +
+                pkgs.lib.makeLibraryPath [
+                  # for jupyter
+                  pkgs.stdenv.cc.cc
+                ];
+            CUDA_PATH = "${pkgs.cudatoolkit}";
+
+            buildInputs = with pkgs; [
+              python312
+              poetry
+
+              cudatoolkit
+            ];
+            shellHook = ''
+              export PS1="(nix-shell)$PS1"
+            '';
+          };
+    };
+}
